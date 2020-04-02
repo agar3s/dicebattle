52734bacc02a514e9e9026bad264c05f1a97c3b1
Agar3s <ekeisco@gmail.com>
2020-04-02 00:37:29 -0500

    implements keep and discard mechanic

--- Dice.gd
+++ Dice.gd
@@ -1,13 +1,27 @@
-extends Sprite
+extends Node2D
 
+signal stopped
 
 var spinning = false setget set_spinning
+# 0, 4, 83, 53, 58, 84, 9, 51, 55, 59, 72, 89
 var faces = [0, 4, 83, 53, 58, 84]
 var fps = 32
 var acc_delta = 0
 
+var keep = false setget set_keep
+var locked = false
+var roll_count = 0
+
+var result = ''
+
 func _ready():
-	pass
+	reset()
+	$Area.connect('input_event', self, 'on_input_event')
+
+
+func on_input_event(_node, _event, _id):
+	if !locked && !spinning && _event is InputEventMouseButton && _event.pressed:
+		self.keep = !keep
 
 
 func _process(delta):
@@ -15,20 +29,62 @@ func _process(delta):
 	acc_delta += delta
 
 	if acc_delta >= 1.0/fps:
-		frame = faces[randi()%6]
-		fps -= 1
+		$Icon.frame = faces[randi()%faces.size()]
+		fps -= 2
 		acc_delta = 0
-		if fps == 4:
-			spinning = false
+		if fps <= 2:
 			fps = 32
-			$AnimationPlayer.play("dice")
-			yield($AnimationPlayer, "animation_finished")
-			$Sprite.show()
-			$Sprite2.show()
-			
+			set_result()
+
+
+func set_result():
+	spinning = false
+	$AnimationPlayer.play('dice')
+	yield($AnimationPlayer, 'animation_finished')
+	
+	match $Icon.frame:
+		0:
+			result = 'skull'
+			self.keep = true
+			self.locked = true
+		4:
+			result = 'curse'
+		83:
+			result = 'attack'
+		53:
+			result = 'heal'
+		58:
+			result = 'destroy_curse'
+		84:
+			result = 'attack'
+	
+	if roll_count >= 3:
+		self.keep = true
+		self.locked = true
+	
+	emit_signal('stopped')
 
 func set_spinning(value):
+	if keep:
+		self.locked = true
+		return
+	
+	roll_count += 1
 	spinning = value
-	if spinning:
-		$Sprite.hide()
-		$Sprite2.hide()
+
+
+func reset():
+	self.locked = false
+	self.keep = false
+	roll_count = 0
+	
+
+
+func set_keep(value):
+	keep = value
+	$Keep_icon.visible = keep
+	if keep:
+		$Back2.color = Color('1f040f')
+	else:
+		$Back2.color = Color('4a565a')
+
--- Game.gd
+++ Game.gd
@@ -5,14 +5,44 @@
 
 extends Node
 
+var roll_count = 0
 
 func _ready():
+	randomize()
 	$Scenario/Player.connect('hit_applied', $Scenario/Enemy, 'apply_damage')
 	$Scenario/Enemy.connect('hit_applied', $Scenario/Player, 'apply_damage')
-	$HUD/Attack_Player.connect('button_down', self, 'roll_dices')
-	$HUD/Attack_Enemy.connect('button_down', $Scenario/Player, 'attack')
+	$HUD/Roll.connect('button_down', self, 'roll_dices')
+	$HUD/Play.connect('button_down', self, 'play_dices')
+	
+	$Scenario/Dices/Dice3.connect('stopped', self, 'dices_stopped')
+	# it doesn't work because when dice 3 is locked it doesnt trigger the event...
 
 
 func roll_dices():
+	if roll_count>=3: return
+	roll_count += 1
+	$HUD/Roll.disabled = true
+	$HUD/Play.disabled = true
+	
 	for dice in $Scenario/Dices.get_children():
 		dice.spinning = true
+		dice.locked = roll_count >= 3
+
+
+func play_dices():
+	# do something
+	# ...
+	# then
+	for dice in $Scenario/Dices.get_children():
+		dice.reset()
+	
+	roll_count = 0
+	$HUD/Roll.disabled = false
+	$HUD/Play.disabled = true
+
+
+func dices_stopped():
+	$HUD/Play.disabled = false
+	if roll_count >= 3: return
+	$HUD/Roll.disabled = false
+
